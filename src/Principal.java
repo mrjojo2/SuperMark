4184f1bed89643309ee8526981cd4950e61c04d7
Mrjojo2 <mrjojo2@live.com>
2022-06-22 15:55:53 -0300

    creando clase principal

---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Usuario us1 = new Usuario("mjojo2", "password");
+		   
+		   System.out.println(us1);
+		   
+			System.out.println("*********************");	   
+			
+			us1.mostrar();
+			
+	}
+
+}
