fd6fb07149a67cc46268b40330388d051ea8202f
Mrjojo2 <mrjojo2@live.com>
2022-06-26 21:40:22 -0300

    menu ingreso y metodo registro

--- src/Principal.java
+++ src/Principal.java
@@ -1,27 +1,55 @@
+import java.util.Scanner;
+
 
 public class Principal {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Usuario us1 = new Usuario("mjojo2", "password");
-		   
-		   System.out.println(us1);
-		   
-			System.out.println("*********************");	   
-			
-			us1.mostrar();
-			
-	}
 	
-	public void menu() {
+	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Bienvenidos a SUPERMARK");
+		System.out.println("A continuación elije una opcion");
+		System.out.println("*******************************************");
 		System.out.println("1. Loguearse");
 		System.out.println("2. Registrarse");
-		System.out.println("0. Salir");
+		System.out.println("3. Salir");
 		System.out.println("Ingrese opcion:");
-		//leen respuesta
+		int op = sc.nextInt();
+		
+		menu(op);
+		
+
+		
+		
+		
+		
+		
+		
+	}
+	
+	public static void menu(int a) {
+				
+	
+		switch( a )
+		{
+		case 1:
+		System.out.println("Ingresando a menu Log-in....");
+		break;
+		case 2:
+		System.out.println("Ingresando a menu registro...");
+			
+			
+		break;
+		case 3:
+		System.out.println("Gracias por visitar SUPERMARK... VUELVA PRONTO");
+		break;
+		default:
+		System.out.println("Valor incorrecto... El valor debe ser entre 1 y 3.");
+		}
+				
+	}		
+		
+		/*
 		
 		switch (op) {
 		case 1: 
@@ -33,8 +61,7 @@ public class Principal {
 			
 		break;
 		default:
-			throw new IllegalArgumentException("Unexpected value: " + key);
-		}
-	}
-
+			throw new IllegalArgumentException("Unexpected value: " + key);    */
+	
 }
+
--- src/Registro.java
+++ src/Registro.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 
-public class Registro {
+public class Registro extends Cliente  {
+
+public Registro(String nombre, String apellido, String domicilio,
+			String telefono, String email, int dni) {
+		super(nombre, apellido, domicilio, telefono, email, dni);
+		
+		registro();
+	}
 
 //realizar metodo de registro
 	
-	public boolean registro (){
+	public void registro (){
 		Scanner leerScanner=new Scanner(System.in);
 		String nombre=" ";
 		String apellido=" ";
@@ -19,24 +26,74 @@ public class Registro {
 		
 		System.out.println("Ingrese");
 		
-	//	while(nombre.equalsIgnoreCase(" ")) {
+	    while(nombre.equalsIgnoreCase(" ")) {
 			System.out.print("Nombre:");
 			nombre=leerScanner.nextLine();
-		//}
-			
+			System.out.println("el nombre ingresado es: " + nombre);
+			}
 			if(nombre.equalsIgnoreCase(" ")) {
-				return false;
+				System.out.println("no ingreso un nombre válido");
 			}
 		
-		while(nombre.equalsIgnoreCase(" ")) {
-			System.out.println("Apellido");
+		while(apellido.equalsIgnoreCase(" ")) {
+			System.out.println("Apellido: ");
 			apellido=leerScanner.nextLine();
+			System.out.println("el Apellido ingresado es: " + apellido);
+		}
+			if(apellido.equalsIgnoreCase(" ")) {
+				System.out.println("no ingreso un Apellido válido");;
 		}
 		
+		    while(domicilio.equalsIgnoreCase(" ")) {
+				System.out.print("Domicilio: ");
+				domicilio=leerScanner.nextLine();
+				System.out.println("el Domicilio ingresado es: " + domicilio);
+				}
+				if(domicilio.equalsIgnoreCase(" ")) {
+				System.out.println("no ingreso una Direccion valida");
+				}
+			
+		while(email.equalsIgnoreCase(" ")) {
+				System.out.print("eMail:");
+				nombre=leerScanner.nextLine();
+				System.out.println("el nombre ingresado es: " + nombre);
+		}
+				if(email.equalsIgnoreCase(" ")) {
+					System.out.println("no ingreso un email valido");
+		        }
+			
+		while(dni == 0) {
+				System.out.print("DNI: ");
+				dni=leerScanner.nextInt();
+				System.out.println("el DNI ingresado es: " + dni);
+				}
+				if(dni == 0) {
+					System.out.println("no ingreso un DNI valido");
+		}
+		
+		while(telefono.equalsIgnoreCase(" ")) {
+		        System.out.print("Telefono: ");
+			    telefono=leerScanner.nextLine();
+				System.out.println("el Telefono ingresado es: " + telefono);
+				}
+				if(telefono.equalsIgnoreCase(" ")) {
+					System.out.println("no ingreso un Telefono válido");
+				}
+				
+		while(clave.equalsIgnoreCase(" ")) {
+					System.out.print("Clave: ");
+					clave=leerScanner.nextLine();
+					System.out.println("La Clave ingresado es: " + clave);
+					}
+					if(clave.equalsIgnoreCase(" ")) {
+						System.out.println("no ingreso una clave valida");;
+					}
+			
+						
 		nuevoCliente=new Cliente(nombre, apellido, domicilio, telefono, email, dni);
 		usuario=new Usuario(email, clave);
 		
-		Conexion db
+	/*	Conexion db
 		db.conectar;
 		db.insert(nombreTabla,columnas,);
 		db.insert()
@@ -46,9 +103,9 @@ public class Registro {
 	}
 	
 	// generar Query String Consulta a la Base de Datos
+	*/
 	
-	
-	
+	}
 } // cierre clase
 
 
