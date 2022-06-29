0cb5fdf9235c5cd4bd9d551b830f1da97dbaf076
CristianFernando88 <gordyfernando@gmail.com>
2022-06-29 00:02:23 -0300

    commit 29/06/2022

--- src/AltaProducto.java
+++ src/AltaProducto.java
@@ -1,10 +1,12 @@
+import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class AltaProducto {
+	final ConexionBD con=new ConexionBD("com.mysql.cj.jdbc.Driver","jdbc:mysql://localhost:3306/supermarket","root","AYATA88");
 
 	// hacer metodo registrar producto
 	public boolean resgitrarProducto() {
-		ConexionBD con=new ConexionBD("com.mysql.cj.jdbc.Driver","jdbc:mysql://localhost:3306/supermarket","root","AYATA88");
+		//ConexionBD con=new ConexionBD("com.mysql.cj.jdbc.Driver","jdbc:mysql://localhost:3306/supermarket","root","AYATA88");
 		
 		Scanner teclado=new Scanner(System.in);
 		
@@ -40,11 +42,15 @@ public class AltaProducto {
 		
 		//valido los datos ingresados
 		if(validarCampos(nombre,descripcion,marca,categoria,stock,costo)==true) {
-			ProductoStock nuevoProducto=new ProductoStock(nombre,descripcion,marca,categoria,Integer.parseInt(stock),Float.parseFloat(costo));
-			con.conectar();//abro conexion
-			con.realizarUpdate(nuevoProducto.generarInsertQuery());//realizo un insert
-			con.cerrarConexion();
-			return true;
+			if(existenciaProducto(nombre,marca)==false) {
+				ProductoStock nuevoProducto=new ProductoStock(nombre,descripcion,marca,categoria,Integer.parseInt(stock),Float.parseFloat(costo));
+				con.conectar();//abro conexion
+				con.realizarUpdate(nuevoProducto.generarInsertQuery());//realizo un insert
+				con.cerrarConexion();
+				return true;
+			}else {
+				return false;
+			}
 		}else {
 			return false;
 		}
@@ -52,7 +58,7 @@ public class AltaProducto {
 	
 	//metodo para validar campos
 	public boolean validarCampos(String nombre,String desc,String marca,String categ,String stock,String costo) {
-		if(nombre.equalsIgnoreCase("")==false && desc.equalsIgnoreCase("")==false && marca.equalsIgnoreCase("")==false && validarNumero(stock)==true && validarNumero(costo)==true) {
+		if(nombre.equalsIgnoreCase("")==false && desc.equalsIgnoreCase("")==false && marca.equalsIgnoreCase("")==false && validarNumeroInt(stock)==true && validarNumeroFloat(costo)==true) {
 			return true;
 		}else  {
 			return false;
@@ -60,7 +66,7 @@ public class AltaProducto {
 	}
 	
 	//metodo para validar un string que es ingresado como numero
-	public boolean validarNumero(String num) {
+	public boolean validarNumeroInt(String num) {
 		
 		try {
 			Integer.parseInt(num);
@@ -68,6 +74,42 @@ public class AltaProducto {
 		} catch (Exception e) {
 			// TODO: handle exception
 			return false;
+		}
+		
+	}
+	
+	public boolean validarNumeroFloat(String num) {
+		
+		try {
+			Float.parseFloat(num);
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			return false;
+		}
+		
+	}
+	
+	public boolean existenciaProducto(String nombre, String marca) {
+		
+		con.conectar();
+		ResultSet resultado=con.consultar("Select * from producto where nombre='"+nombre+"' and marca='"+marca+"';");
+		int cont=0;
+		try {
+			while(resultado.next()) {
+				cont++;
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		con.cerrarConexion();
+		if(cont==0) {
+			return false;
+		}else {
+			return true;
 		}
 	}
 	
--- src/Principal.java
+++ src/Principal.java
@@ -8,6 +8,37 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		
+		/*AltaProducto nuevoProducto=new AltaProducto();
+		
+		System.out.println(nuevoProducto.existenciaProducto("DETERGENTE","ALA"));*/
+		
+		
+		
+		
+		/*ConexionBD con=new ConexionBD("com.mysql.cj.jdbc.Driver","jdbc:mysql://localhost:3306/supermarket","root","AYATA88");
+		con.conectar();
+		ResultSet resultado=con.consultar("Select * from producto where nombre='Harina' and marca='favorita';");
+		int cont=0;
+		try {
+			while(resultado.next()) {
+				int id=resultado.getInt("idProducto");
+				String nombreString=resultado.getString("nombre");
+				String marcaString=resultado.getString("Marca");
+				
+				System.out.print(" Codigo:"+id);
+				System.out.print(" Nombre:"+nombreString);
+				System.out.print(" Marca:"+marcaString);
+				System.out.println();
+				cont++;
+			}
+			System.out.println(cont);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		con.cerrarConexion();*/
+		
 		AltaProducto nuevoProducto=new AltaProducto();
 		
 		if(nuevoProducto.resgitrarProducto()) {
